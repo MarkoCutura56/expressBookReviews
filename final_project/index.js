3d80486da65aa71c5a85b33eae25504fc43949b1
MarkoCutura56 <107407863+MarkoCutura56@users.noreply.github.com>
2024-03-06 14:37:20 +0100

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -5,18 +5,23 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
 app.use(express.json());
+app.get("/",(req,res,next)=>
+{
+    res.send("Welcome to Marko server");
+   next();
+})
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-});
+// app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+// app.use("/customer/auth/*", function auth(req,res,next){
+// //Write the authenication mechanism here
+// });
  
-const PORT =5000;
-
-app.use("/customer", customer_routes);
+const PORT =80;
 app.use("/", genl_routes);
+//app.use("/customer", customer_routes);
+
 
 app.listen(PORT,()=>console.log("Server is running"));
